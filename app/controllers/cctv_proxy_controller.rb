426fa687580e7eb28b8ff898cfe194a9b6b156d4
MOSapeizer <k471352@gmail.com>
2016-05-13 12:26:32 +0800

    fix timestamp length

--- app/controllers/cctv_proxy_controller.rb
+++ app/controllers/cctv_proxy_controller.rb
@@ -7,11 +7,21 @@ class CctvProxyController < ApplicationController
     url = 'http://dfm.swcb.gov.tw/debrisFinal/QueryCCDRange.asp'
     query = { Stationid: station, CCDID: ccd, mode: 1 }
     packet = XmlRequest.new(url)
-    response = packet.get( query ) do | response |
-      next { index: response.scan(/\d{8,10}/),
-             timestamp: response.scan(/\d{4}\/\d{1,2}\/\d{1,2}\s\d{1,2}[:]\d{1,2}[:]\d{1,2}/).slice!(-4, 4) }
+    json_response = packet.get( query ) do | response |
+      next parse_to_json(response)
     end
 
-    render json: response
+    render json: json_response
+  end
+
+  private
+
+  def parse_to_json(response)
+    index = response.scan(/\d{8,10}/)
+    timestamp = response.scan(/\d{4}\/\d{1,2}\/\d{1,2}\s\d{1,2}[:]\d{1,2}[:]\d{1,2}/)
+    timestamp.slice!(-4, 4)
+
+    return { index: index, timestamp: timestamp }
+
   end
 end
